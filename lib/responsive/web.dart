e521ec9376705afa5feae6def278c082fe9e1745
Asmaa Abdelfattah <109594626+AsmaaAbdelfattah@users.noreply.github.com>
2026-01-26 16:24:16 +0200

    add bottom navgation view for mobile and for web customize app bar to do same as bottom navigation

--- lib/responsive/web.dart
+++ lib/responsive/web.dart
@@ -11,8 +11,22 @@ class _WebUIState extends State<WebUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title:  Text("web screen")),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(icon: Icon(Icons.home), onPressed: () {}),
+          IconButton(icon: Icon(Icons.search), onPressed: () {}),
+          IconButton(icon: Icon(Icons.add), onPressed: () {}),
+          IconButton(icon: Icon(Icons.favorite), onPressed: () {}),
+          IconButton(icon: Icon(Icons.person), onPressed: () {}),
+        ],
+        title: Image.asset(
+          "lib/imgs/Instagram_logo.svg",
+          color: Colors.white,
+          height: 45,
+        ),
+      ),
     );
   }
 }
